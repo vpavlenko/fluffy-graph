224674f6b7de7b1da73dab3e601d77863b5fdc39
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-06-23 23:00:28 +0400

    add green feedback on correct answer

--- js/game.js
+++ js/game.js
@@ -7,7 +7,8 @@ var new_level = undefined;
 var game_is_on = true;
 var timer = 0;
 
-var MAX_LEVEL = 11;
+// var MAX_LEVEL = levels.length - 1;
+// console.log('num levels = ', MAX_LEVEL)
 
 /*
  * Utilities
@@ -20,8 +21,16 @@ function random_randrange(n) {
 function random_randrange_excluded(n, k) {
     while (true) {
         var i = random_randrange(n)
-        if (i != k) {
-            return i;
+        if (k instanceof Array) {
+            console.log('excluded Array', JSON.stringify(k))
+            if ($.inArray(i, k) == -1) {
+                console.log('return ', i);
+                return i;
+            }
+        } else {
+            if (i != k) {
+                return i;
+            }
         }
     }
 }
@@ -97,27 +106,40 @@ function new_game() {
 }
 
 function win() {
-    correct_answers++;
-    add_to_timer(3);
+    var current_correct_answers = ++correct_answers;
+    
+    $('#counter').addClass('counter-win');
+    setTimeout(function() {
+        if (current_correct_answers == correct_answers) {
+            $('#counter').removeClass('counter-win');
+        }
+    }, 300);
+    
+    add_to_timer(5 + Math.round(correct_answers / 5));
     $("#counter").text(correct_answers);
     switch_to_new_level();
 }
 
 function lose() {
     game_is_on = false;
-    $('#timer').text("lost");
+    $('#timer').html("<span class='lost'>no</span>");
 }
 
 function generate_new_level() {
-    var level = Math.min(Math.round(Math.sqrt(2.5 * correct_answers)), MAX_LEVEL);
+    var level = Math.min(correct_answers, levels.length - 1);
+    // var level = Math.min(Math.round(Math.sqrt(2.5 * correct_answers)), MAX_LEVEL);
     console.log('level: ' + level);
-    var num_choices = (correct_answers >= 15) ? 3 : 2;
+    var num_choices = (correct_answers >= 5) ? 3 : 2;
 
     var reference_graph = random_randrange(levels[level].length);
 
     var choices = []
     for (var i = 0; i < num_choices - 1; ++i) {
-        var choice = random_randrange_excluded(levels[level].length, reference_graph)
+        if (choices.length + 1 < levels[level].length) {
+            var choice = random_randrange_excluded(levels[level].length, choices.concat([reference_graph]))
+        } else {
+            var choice = random_randrange_excluded(levels[level].length, reference_graph);
+        }
         choices.push(levels[level][choice]);
     }
 
@@ -173,7 +195,11 @@ $(function() {
     new_game();
 
     $(document).on('click', '.choice', function() {
-       handle_choice($(this).attr("data-number"));
+        handle_choice($(this).attr("data-number"));
+    });
+
+    $(document).on('keypress', 'html', function(event) {
+        handle_choice(event.keyCode - 49);
     });
 
     setInterval(function() {
