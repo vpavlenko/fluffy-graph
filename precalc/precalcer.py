7302a137a6e2bb929faf3bad968fe4d43e00a283
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-06-23 22:59:34 +0400

    #4: deny small angles between adjacent edges

--- precalc/precalcer.py
+++ precalc/precalcer.py
@@ -5,8 +5,9 @@ import networkx as nx
 import sys
 import json
 import random
+from math import pi, acos, sqrt
 
-
+random.seed(0)
 
 height = 400;
 width = 400;
@@ -14,6 +15,10 @@ border = 30;
 delta = 25;
 EPSILON = 1e-6;
 MIN_CROSS_PRODUCT = height * width / 50;
+LAYOUT_ITERATIONS = 60
+MINIMAL_ANGLE_BETWEEN_EDGES = 10
+MAX_LAYOUT_RETRY = 50
+
 
 def random_with_grid(limit, border, delta):
     # Generates random number in [border; border + delta; border + 2 * delta; ... ; limit - border)
@@ -102,16 +107,74 @@ def array_to_list(a):
     return [int(round(x + border)) for x in a]
 
 
+def norm(u):
+    return sqrt(u[0] ** 2 + u[1] ** 2)
+
+
+def angle(u, v):
+    arg = dot_product(u, v) / (1. * norm(u) * norm(v)) - EPSILON
+    try:
+        res = acos(arg)
+        # print(res)
+        return acos(arg)
+    except Exception as e:
+        print(arg)
+        raise e
+
+
+def degrees_to_radians(degrees):
+    return degrees / 180. * pi
+
+
+def good_edges_layout(e, f, pos):
+    if e[0] == f[0]:
+        pass
+    elif e[0] == f[1]:
+        f = f[::-1]
+    elif e[1] == f[0]:
+        e = e[::-1]
+    else:  # e[1] == f[1]
+        e = e[::-1]
+        f = f[::-1]
+    assert e[0] == f[0] 
+    u = edge_to_vector(e, pos)
+    v = edge_to_vector(f, pos)
+    return dot_product(u, v) < 0 or (angle(u, v) >= degrees_to_radians(MINIMAL_ANGLE_BETWEEN_EDGES))
+
+
+print('degrees_to_radians = ', degrees_to_radians(MINIMAL_ANGLE_BETWEEN_EDGES))
+
+
+def good_layout(g, pos):
+    # 1. Check that there are no adjacent edges with small angle between them
+    for e in g.edges():
+        for f in g.edges():
+            if len(set(e + f)) == 3:
+                if not good_edges_layout(e, f, pos):
+                    print('Found bad angle')
+                    return False
+    return True
+
+
 def draw_graph_nx(g):
-    pos = nx.spring_layout(g, scale=height - 2 * border, iterations=45)
-    return [array_to_list(pos[i]) for i in range(len(g.nodes()))]
+    j = 0
+    while True:
+        j += 1
+        pos = nx.spring_layout(g, scale=height - 2 * border, iterations=LAYOUT_ITERATIONS)
+        pos = [array_to_list(pos[i]) for i in range(len(g.nodes()))]
+        if good_layout(g, pos):
+            break
+        if j > MAX_LAYOUT_RETRY:
+            return None
+    return pos
 
 
 def drawings(g, n):
-    return [draw_graph_nx(g) for i in range(n)]
-
-
-
+    array = [draw_graph_nx(g) for i in range(n)]
+    if None in array:
+        return None
+    else:
+        return array
 
 
 inp_filename = sys.argv[1]
@@ -128,6 +191,7 @@ with open(out_filename, 'w') as ouf:
         gr = {
             'n': len(g.nodes()),
             'e': g.edges(),
+            'm': len(g.edges()),
             't': sum(nx.triangles(g).values()) / 3,  # number of triangles
             'b': len(list(nx.biconnected_components(g))),  # number of biconnected components
             'mind': min(degrees),
@@ -135,6 +199,8 @@ with open(out_filename, 'w') as ouf:
         }
 
         gr['i'] = drawings(g, 10)
-
-        print(json.dumps(gr, separators=(',',':')), file=ouf)
-        print(json.dumps(gr, separators=(',',':')))
+        if gr['i'] is not None:
+            print(json.dumps(gr, separators=(',',':')), file=ouf)
+        else:
+            print('EVERY LAYOUT IS BAD')
+        # print(json.dumps(gr, separators=(',',':')))
